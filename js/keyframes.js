e66f1b388f12975d50c7196a125ff35f86537f23
unknown <dantim1974@hotmail.com>
2023-03-08 23:06:54 +0100

    section keyframe correct  commit

--- js/keyframes.js
+++ js/keyframes.js
@@ -4,12 +4,12 @@ let cardTwo = document.querySelector('.card2')
 let cardThree = document.querySelector('.card3')
 let yLastPosition;
 let largeur = sectionWidth.offsetWidth
-console.log(largeur);
+
 window.addEventListener('scroll', ()=>{
     if(window.scrollY > 2200 && largeur < 768){
         cardOne.classList.add('slideinright')
     }
-    if(window.scrollY > 1500 && largeur < 992){
+    if(window.scrollY > 1600 && largeur > 768){
         cardOne.classList.add('slideinright')
     }
     if(window.scrollY > 1300 && largeur > 992){
@@ -18,10 +18,10 @@ window.addEventListener('scroll', ()=>{
     yLastPosition = window.scrollY;
 })
 window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 2500 && largeur < 768){
+    if(window.scrollY > 2600 && largeur < 768){
         cardTwo.classList.add('slideinleft')
     }
-    if(window.scrollY > 1600 && largeur < 992){
+    if(window.scrollY > 2000 && largeur > 768){
         cardTwo.classList.add('slideinleft')
     }
     if(window.scrollY > 1400 && largeur > 992){
@@ -30,10 +30,10 @@ window.addEventListener('scroll', ()=>{
     yLastPosition = window.scrollY;
 })
 window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 2700 && largeur < 768){
+    if(window.scrollY > 3000 && largeur < 768){
         cardThree.classList.add('slideinrighttwo')
     }
-    if(window.scrollY > 1700 && largeur < 992){
+    if(window.scrollY > 2400 && largeur > 768){
         cardThree.classList.add('slideinrighttwo')
     }
     if(window.scrollY > 1500 && largeur > 992){
