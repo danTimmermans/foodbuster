c73634a19adb1aa7f61d70e8340b6d47309aa009
unknown <dantim1974@hotmail.com>
2023-03-11 12:36:18 +0100

    all home commit

--- js/jauge.js
+++ js/jauge.js
@@ -6,7 +6,21 @@ var percentTwo = 70;
 var percentThree = 100;
 
 window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 6000){
+    if(window.scrollY > 6000 && largeur < 768){
+        setTimeout(function(){
+            var DOMStyle =getComputedStyle(root);
+            root.style.setProperty('--nb-percent', percent);
+            root.style.setProperty('--nb-percent-str', '"'+percent+'%"')
+        }, 1000);
+    }
+    if(window.scrollY > 5200 && largeur > 768){
+        setTimeout(function(){
+            var DOMStyle =getComputedStyle(root);
+            root.style.setProperty('--nb-percent', percent);
+            root.style.setProperty('--nb-percent-str', '"'+percent+'%"')
+        }, 1000);
+    }
+    if(window.scrollY > 3400 && largeur > 992){
         setTimeout(function(){
             var DOMStyle =getComputedStyle(root);
             root.style.setProperty('--nb-percent', percent);
@@ -16,7 +30,21 @@ window.addEventListener('scroll', ()=>{
     yLastPosition = window.scrollY;
 })
 window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 6600) {
+    if(window.scrollY > 6600 && largeur < 768) {
+        setTimeout(function(){
+            var DOMStyle =getComputedStyle(root);
+            root.style.setProperty('--nb-percent-2', percentTwo);
+            root.style.setProperty('--nb-percent-str-2', '"'+percentTwo+'%"')
+        }, 1000);
+    }
+    if(window.scrollY > 5700 && largeur > 768){
+        setTimeout(function(){
+            var DOMStyle =getComputedStyle(root);
+            root.style.setProperty('--nb-percent-2', percentTwo);
+            root.style.setProperty('--nb-percent-str-2', '"'+percentTwo+'%"')
+        }, 1000);
+    }
+    if(window.scrollY > 3400 && largeur > 992){
         setTimeout(function(){
             var DOMStyle =getComputedStyle(root);
             root.style.setProperty('--nb-percent-2', percentTwo);
@@ -26,7 +54,21 @@ window.addEventListener('scroll', ()=>{
     yLastPosition = window.scrollY;
 })
 window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 6900) {
+    if(window.scrollY > 6900 && largeur < 768) {
+        setTimeout(function(){
+            var DOMStyle =getComputedStyle(root);
+            root.style.setProperty('--nb-percent-3', percentThree);
+            root.style.setProperty('--nb-percent-str-3', '"'+percentThree+'%"')
+        }, 1000);
+    }
+    if(window.scrollY > 6000 && largeur > 768){
+        setTimeout(function(){
+            var DOMStyle =getComputedStyle(root);
+            root.style.setProperty('--nb-percent-3', percentThree);
+            root.style.setProperty('--nb-percent-str-3', '"'+percentThree+'%"')
+        }, 1000);
+    }
+    if(window.scrollY > 3400 && largeur > 992){
         setTimeout(function(){
             var DOMStyle =getComputedStyle(root);
             root.style.setProperty('--nb-percent-3', percentThree);
